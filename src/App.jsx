21481047c01c559850cf3087e3e577d6a21c92b6
Garongan <alvindo56@gmail.com>
2023-08-31 12:43:40 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <div className="header">
+          <h1>Movies Hunter</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
